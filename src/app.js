779f4ae01db5f8277dea67dd8b6b05f04f1d9184
Nicolas Siqueira <nsiquei1@gmail.com>
2020-02-02 15:43:30 -0300

    installation and configuration of eslint to work and save automatically, not working yet

--- src/app.js
+++ src/app.js
@@ -2,18 +2,17 @@ import express from 'express';
 import routes from './routes';
 
 class App {
-constructor(){
-  this.server= express();
-  
-  this.middlewares();
-  this.routes();
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.routes();
   }
 
-  middlewares(){
+  middlewares() {
     this.server.use(express.json());
   }
 
-  routes(){
+  routes() {
     this.server.use(routes);
   }
 }
